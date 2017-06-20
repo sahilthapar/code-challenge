2afa1531e11b7a0329f8665efbbecfcc8412306b
Sahil Thapar <sahilthapar1992@gmail.com>
2017-06-20 15:46:06 -0400

    Yet more documentation

--- src/datastore.py
+++ src/datastore.py
@@ -1,6 +1,21 @@
 from utils import parseDate
 
 class Datastore():
+  """Datastore class
+
+    Stores in-memory event data
+
+    Args:
+
+    Attributes:
+        latest_time (datetime): Latest event time recorded
+        customers (dict): {customer_id: customer} Contains all customers info
+        orders (dict): {key: order} Contains all orders info
+        site_visits (dict): {key: site_visit} Contains all site visits information
+        images (dict): {key: image} Contains all images info
+        events (dict): {key: event} Contains all other event types
+
+    """
   def __init__(self):
     self.latest_time = parseDate("1970-01-01T00:00:00.000000")
     self.customers = {}
@@ -10,19 +25,49 @@ class Datastore():
     self.events = {}
 
   def updateLatestTime(self, event):
+    """
+    Updates latest time of the datastore
+    :param event: 
+    :return: 
+    """
     self.latest_time = max(self.latest_time, parseDate(event.get('event_time')))
 
   def add_customer(self, customer):
+    """
+    Adds a new customer to the datastore
+    :param event: 
+    :return: 
+    """
     self.customers[customer.customer_id] = customer
 
   def add_order(self, order):
+    """
+    Adds a new order to the datastore
+    :param event: 
+    :return: 
+    """
     self.orders[order.key] = order
 
   def add_site_visit(self, site_visit):
+    """
+    Adds a new site visit to the datastore
+    :param event: 
+    :return: 
+    """
     self.site_visits[site_visit.key] = site_visit
 
   def add_image(self, image):
+    """
+    Adds a new image to the datastore
+    :param event: 
+    :return: 
+    """
     self.images[image.key] = image
 
   def add_event(self, event):
+    """
+    Adds a new event (other event types) to the datastore
+    :param event: 
+    :return: 
+    """
     self.events[event.key] = event
--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,19 @@ import datetime
 
 
 def parseDate(date_str):
+  """
+  Converts date string to datetime object, Expects ISO8601 format
+  :param date_str: 
+  :return: datetime object
+  """
   date_format = "%Y-%m-%dT%H:%M:%S.%f"
   return datetime.datetime.strptime(date_str.replace("Z", ""), date_format)
 
 
 def parseAmount(amount_str):
+  """
+    Extracts amount value from amount string
+    :param amount_str: 
+    :return: amount (float)
+  """
   return float(amount_str.replace(' USD', ''))
